2575711a5697fc0e2828553d2b68334cf3a80acd
aarakaki <aarakaki@techinisys.com>
2019-07-17 15:31:04 -0300

    Se agrega README.md y .gitignore

---
+++ NODE-JS for dummies/03-node-base/README.md
@@ -0,0 +1,16 @@
+#Curso en el que se aprenden las bases de node
+
+*Importar clases externas o creadas por nosotros
+*npm init (Configuracion del package.json)
+*npm install (Instalacion de paquetes externos Ej: yargs)
+
+
+
+##YARGS
+-Es una librería que nos ayuda en la definicion de los parametros de entrada. A su vez, nos permite setear alias, si es requerido, un valor por defecto, etc.
+-Fijarse en app.js el código y los comentarios}
+
+######<u>Modo de uso</u>
+node app -l 10 -b 17 
+**<Descripcion>**
+En este ejemplo estamos indicando que nos muestre la tabla del 17 del 1 al 10
--- NODE-JS for dummies/03-node-base/app.js
+++ NODE-JS for dummies/03-node-base/app.js
@@ -1,15 +1,34 @@
+const argv = require('yargs')
+    .command('listar'/*Nombre que se utilizará para llamarlo en la consola*/, 'Imprime en consola la tabla de multiplicar',{
+        base: {
+            demand: true, //Campo obligatorio
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+.argv;
+
+
 const {crearArchivo} = require("./multiplicar/multiplicar");
 
 // let base = 5;
 
-let argv = process.argv;
-let base = argv[2]; //3er casilla del vecto = 1ra variable que se le pasa desde node
+let argv2 = process.argv;
 
-console.log(base);
+console.log("Limite:", argv.limite)
+// console.log(argv);
+// console.log(argv2);
 
-crearArchivo(base)
-    .then(archivo => console.log(`El archivo ${archivo} fue creado satisfactoriamente`))
-    .catch(err => console.log(err));
+// let base = argv[2]; //3er casilla del vecto = 1ra variable que se le pasa desde node
+
+// console.log(base);
+
+// crearArchivo(base)
+//     .then(archivo => console.log(`El archivo ${archivo} fue creado satisfactoriamente`))
+//     .catch(err => console.log(err));
 
 
 //const tabla3 = fs.readFile("./tablas/tabla-3.txt", );
